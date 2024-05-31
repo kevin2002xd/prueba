32f5fe33a5d95360318dfa5008c8f09b3bcfc968
kevin2002xd <kevin_jl@tesch.edu.mx>
2024-05-30 19:35:56 -0600

    Update blocking.js
    jeje

--- blocking.js
+++ blocking.js
@@ -2,3 +2,4 @@ const fs = require('fs');
 const data = fs.readFileSync('C:\Users\Dell\Documents\ejemplo.txt');
 console.log(data);
 //moreWorck(); C:\Users\Dell\Documents\ejemplo.txt
+// I like ferPoronga
